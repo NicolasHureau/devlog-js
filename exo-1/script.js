246d69db50e0c11f943de4613091641d7abf3a67
Nicolas Hureau <116349392+NicolasHureau@users.noreply.github.com>
2023-05-23 14:27:23 +0200

    Ex 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,25 @@
 // you can write js here
 console.log('hello from file');
+
+// const kelvins = 294;
+const kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+console.log(kelvins);
+
+const celsius = kelvins - 273; // on déclare une constante nommé celsuis qui prend en valeur le résultat de la valeur de la constante kelvins moin 273.
+console.log(celsius);
+
+const fahrenheits = celsius * (9/5) + 32; // on déclare une constante nommé fahrenheits qui prend en valeur le résultat de la valeur de la constante celsius multiplié par neuf cinquième plus trente-deux. 
+console.log(fahrenheits);
+
+// il y as plusieurs façon d'appliquer .floor() :
+const fahrenheitsPrime = Math.floor(celsius * (9/5) + 32);
+console.log(fahrenheitsPrime);
+
+// ou 
+console.log(Math.floor(fahrenheits));
+
+// ou encore
+function rounded(temperature){
+    return Math.floor(temperature);
+}
+console.log(rounded(fahrenheits));

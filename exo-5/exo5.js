2b198f9314c3061b72b631f06b7a87876ba1a02c
Nicolas Hureau <116349392+NicolasHureau@users.noreply.github.com>
2023-05-24 15:00:55 +0200

    Chantons comme des baleines!

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,22 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = prompt("Que voulez-vous traduire en baleine?");
+const vowels = ["a","e","i","o","u","y"];
+let resultArray = new Array;
+
+for(i=0;i<input.length;i++){
+    console.log(input[i]);
+    // if(vowels.indexOf(input[i])!==-1){
+    //     resultArray.push(input[i]);
+    // }
+    for(j=0;j<vowels.length;j++){
+        if(input[i] == vowels[j]){
+            resultArray.push(input[i]);
+        }
+    }
+}
+
+console.log(resultArray);
+console.log(resultArray.join("").toUpperCase());
